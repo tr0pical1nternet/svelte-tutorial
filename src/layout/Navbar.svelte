88325e1f83d1655a30d47ef6d667c8c0454d6f81
Charles Robertson <admin@cocohost.co>
2019-09-04 20:00:55 -0500

    Initial commit

---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Link } from 'svelte-routing';
+</script>
+
+<style>
+</style>
+
+<nav>
+  <div class="nav-wrapper">
+    <div class="container">
+      <Link to='/'><span class="brand-logo left">Svelte Tutorial</span></Link>
+      <ul id="nav-mobile" class="right">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </div>
+  </div>
+</nav>
